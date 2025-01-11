3bd05a94e6fac00c980848447e537d7fad369b62
Tank <63246618+coolrobby@users.noreply.github.com>
2025-01-11 22:23:20 +0800

    Update show_the_words.py

--- show_the_words.py
+++ show_the_words.py
@@ -16,8 +16,15 @@ task_dict = {task: idx for idx, task in enumerate(tasks)}
 st.sidebar.title('Navigation')
 selected_task = st.sidebar.radio('Choose a Task', tasks)
 
+# 使用session_state来跟踪选中的任务
+if 'selected_task' not in st.session_state:
+    st.session_state.selected_task = tasks[0]  # 默认选中第一个任务
+
+# 更新session_state中的selected_task
+st.session_state.selected_task = selected_task
+
 # 获取选择的任务对应的数据
-task_idx = task_dict[selected_task]
+task_idx = task_dict[st.session_state.selected_task]
 group_name = df.iloc[task_idx, 0]  # 获取对应组的名称
 group_data = grouped.get_group(group_name)
 
@@ -26,4 +33,3 @@ st.title(f"Group: {group_name}")
 for index, row in group_data.iterrows():
     st.markdown(f"### {row[2]}")  # 第三列内容，作为<h2>
     st.markdown(f"{row[3]}")      # 第四列内容，作为<p></p><hr>
-
